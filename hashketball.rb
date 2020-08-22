b1ee4ca4647837d83ea709935dde04fecc664384
Willem A Duiker <duikhead2@gmail.com>
2020-08-22 20:07:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -151,7 +151,7 @@ def team_colors(team)
   game = game_hash
   game_hash.each do |key,value|
     if value[:team_name] == team
-      puts value[:team_name][:colors]
+      puts value[:colors]
     end
   end
 end
