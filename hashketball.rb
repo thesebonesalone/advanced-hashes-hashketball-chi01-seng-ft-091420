e9869543925fc6f1faf46807d1c0e4493354a46d
Willem A Duiker <duikhead2@gmail.com>
2020-08-22 19:57:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,10 +129,7 @@ end
 def num_points_scored(name)
   game = game_hash
   game_hash.each do |key, value|
-    puts value
-    key[:players].each do |index|
-      puts index
-    end
+    puts key
   end
 end
 
